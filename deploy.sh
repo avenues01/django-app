08169054cb854ea18b7cbee6abd32af4e87b4d73
Vinay Arora <vinay.arora7171@hotmail.com>
2020-12-17 23:32:45 +0530

    Added docker start

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ python3 manage.py migrate
 python3 manage.py collectstatic
 echo yes
 sudo docker-compose stop
+sudo docker-compose up --build
 sudo systemctl restart nginx
 sudo systemctl restart gunicorn
