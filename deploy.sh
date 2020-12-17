26e253df12e9dfdc8bf883a273cbd53154739986
Vinay Arora <vinay.arora7171@hotmail.com>
2020-12-17 23:16:30 +0530

    Added echo command

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ sudo pip3 install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic
+echo yes
 sudo docker-compose stop
 sudo systemctl restart nginx
 sudo systemctl restart gunicorn
